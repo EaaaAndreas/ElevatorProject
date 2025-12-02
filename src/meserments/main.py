e74c4efebe856110c3f1dec7e1aff61928736879
andre <eaa25ansn@students.eaaa.dk>
2025-12-02 12:12:17 +0100

    Bugfixing

--- src/meserments/main.py
+++ src/meserments/main.py
@@ -1,30 +1,33 @@
 import random
-import json 
+import json
+import os
 
-def messure():
+def messure() -> int:
     return random.randint(0, 10000)
 
-def saving(etage, distence):
-    data = {
-        'etage': etage,
-        'distence': distence
-    }
+def saving(etage, distance):
+    if os.path.exists("./data.json"):
+        with open('data.json', "r") as file:
+            data = json.load(file)
+    else:
+        data = {}
+    data.update({etage: distance})
 
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
-    
-    return
 
 def floor():
     try:
         with open("data.json", "r") as f:
             data = json.load(f)
-            for i in data:
-                floor = i['etage']
-                afstand = i['distence']
-            return floor, afstand
+            return data
     except FileNotFoundError:
         print("No data.json file found yet.")
+
+def get_floor(etage) -> int:
+    distance = ...
+    # raise error when no data.json file
+    return distance
 
 def main():
     while True:
@@ -33,8 +36,8 @@ def main():
         saving(etage, distence)
         print('data saved')
         print('floor er are on')
-        info = floor()
-        print(info)
+        for etage, distance in floor().items():
+            print(f"{etage}: {distance}")
         
 main()
 
