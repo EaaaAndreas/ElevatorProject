30f3cd1654e92d406d1de809776881096aa9a011
andre <eaa25ansn@students.eaaa.dk>
2025-12-02 12:03:33 +0100

    Bugfixing and commenting on definitions. Added get_current_floor function to see what floor the elevator is at now. (unfinished)

--- src/motor/movement.py
+++ src/motor/movement.py
@@ -2,5 +2,13 @@
 from .motor import *
 
 
+def get_current_floor():
+    """
+    Returns the current floor of the elevator (Only if the elevator is parked at a certain floor).
+    :return:
+    :rtype:
+    """
+    pass
+
 def go_to_floor(floor:int):
     pass
